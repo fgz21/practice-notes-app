4e203be8501e390b56b43a4bc894e853d3dce645
Erick Ramirez <77713259+Erickgiber@users.noreply.github.com>
2023-05-31 10:50:23 -0400

    New improvements

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 ///button primary
 
-const button = document.getElementById('menu-1');
-const dropdown = document.querySelector('.origin-top-right-1');
+const button = document.getElementById("menu-1");
+const dropdown = document.querySelector(".origin-top-right-1");
 
-button.addEventListener('click', () => {
-    dropdown.classList.toggle('hidden');
+button.addEventListener("click", () => {
+  dropdown.classList.toggle("hidden");
 });
-
 
 //button register secundary
 
-const button2 = document.getElementById('menu-2');
-const dropdown2 = document.querySelector('.origin-top-right-2');
+const button2 = document.getElementById("menu-2");
+const dropdown2 = document.querySelector(".origin-top-right-2");
 
-button2.addEventListener('click', () => {
-    dropdown2.classList.toggle('hidden');
+button2.addEventListener("click", () => {
+  dropdown2.classList.toggle("hidden");
 });
-
