77c1d91dc3a3d84a03604dd7c0a80083f191cc81
fgz21 <flowhister30@gmail.com>
2023-05-30 21:22:15 -0400

    new commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+///button primary
+
+const button = document.getElementById('menu-1');
+const dropdown = document.querySelector('.origin-top-right-1');
+
+button.addEventListener('click', () => {
+    dropdown.classList.toggle('hidden');
+});
+
+
+//button register secundary
+
+const button2 = document.getElementById('menu-2');
+const dropdown2 = document.querySelector('.origin-top-right-2');
+
+button2.addEventListener('click', () => {
+    dropdown2.classList.toggle('hidden');
+});
+
